49e5c4f51eb1c59d7e8a143d9013c09df942bb7c
Jan-Peter Richters <richters@berlinheals.de>
2021-02-12 09:37:52 +0100

    change URL to use https

--- ftdi-drivers/tools/chocolateyinstall.ps1
+++ ftdi-drivers/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageArgs = @{
   packageName            = "$env:chocolateyPackageName"
-  url                    = 'http://www.ftdichip.com/Drivers/CDM/CDM21228_Setup.zip'
+  url                    = 'https://www.ftdichip.com/Drivers/CDM/CDM21228_Setup.zip'
   checksum               = '764A3C033FE4F29488A80EA92E7A0D284B8FC8ECE77BEF0F17D203BF8A6FEDFD'
   checksumType           = 'sha256'
   UnzipLocation          = "$env:TMP"
